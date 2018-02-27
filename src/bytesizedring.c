a68cc8a5b1da22f113cdd9acc37ad9af416a6b6d
brenton <brenton.d.walker@gmail.com>
2018-02-27 12:24:59 +0100

    add a wrapper for rte_ring that limits the number of bytes in the packets in the ring.

---
+++ src/bytesizedring.c
@@ -0,0 +1,68 @@
+#include <rte_config.h>
+#include <rte_common.h>
+#include <rte_ring.h>
+#include "bytesizedring.h"
+
+// DPDK SPSC bounded ring buffer
+/*
+ * This wraps the DPDK SPSC bounded ring buffer into a structure whose capacity
+ * limits the number of bytes it can hold.
+ */
+
+
+struct bs_ring* create_bsring(uint32_t capacity, int32_t socket) {
+	static volatile uint32_t ring_cnt = 0;
+	int count = capacity/60;
+	char ring_name[32];
+	struct bs_ring* bsr = (struct bs_ring*)malloc(sizeof(struct bs_ring*));
+	bsr->capacity = capacity;
+	sprintf(ring_name, "mbuf_bs_ring%d", __sync_fetch_and_add(&ring_cnt, 1));
+	bsr->ring = rte_ring_create(ring_name, count, socket, RING_F_SP_ENQ | RING_F_SC_DEQ);
+	bsr->used = 0;
+	if (! bsr->ring) {
+		free(bsr);
+		return NULL;
+	}
+	return bsr;
+}
+
+int bsring_enqueue_bulk(struct bs_ring* bsr, struct rte_mbuf** obj, int n) {
+	int num_added = 0;
+	int i = 0;
+	for (i=0; i<n; i++) {
+		num_added += bsring_enqueue(bsr, obj[i]);
+	}
+	return num_added;
+}
+
+int bsring_enqueue(struct bs_ring* bsr, struct rte_mbuf* obj) {
+	if (bsr->used < bsr->capacity) {
+		if (rte_ring_sp_enqueue(bsr->ring, obj) == 0) {
+			bsr->used += obj->pkt_len;
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int bsring_dequeue_bulk(struct bs_ring* bsr, struct rte_mbuf** obj, int n) {
+	int num_dequeued = rte_ring_sc_dequeue_bulk(bsr->ring, (void**)obj, n, NULL);
+	int i = 0;
+	for (i=0; i<num_dequeued; i++) {
+		bsr->used -= obj[i]->pkt_len;
+	}
+	return num_dequeued;
+}
+
+int bsring_count(struct bs_ring* bsr) {
+	return rte_ring_count(bsr->ring);
+}
+
+int bsring_capacity(struct bs_ring* bsr) {
+	return bsr->capacity;
+}
+
+int bsring_bytesused(struct bs_ring* bsr) {
+	return bsr->used;
+}
+
---
+++ src/bytesizedring.h
@@ -0,0 +1,35 @@
+#ifndef MG_BYTESIZEDRING_H
+#define MG_BYTESIZEDRING_H
+
+#include <rte_config.h>
+#include <rte_common.h>
+#include <rte_ring.h>
+#include <rte_mbuf.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+struct bs_ring
+{
+	struct rte_ring* ring;
+	int capacity;
+	int used;
+};
+
+struct bs_ring* create_bsring(uint32_t capacity, int32_t socket);
+int bsring_enqueue_bulk(struct bs_ring* bsr, struct rte_mbuf** obj, int n);
+int bsring_enqueue(struct bs_ring* bsr, struct rte_mbuf* obj);
+int bsring_dequeue_bulk(struct bs_ring* bsr, struct rte_mbuf** obj, int n);
+int bsring_count(struct bs_ring* r);
+int bsring_capacity(struct bs_ring* bsr);
+int bsring_bytesused(struct bs_ring* bsr);
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
+
