55fbf89433ed0b3781fc0639ea261a91b0f53721
brenton <brenton@filab.uni-hannover.de>
2020-05-14 12:43:58 +0200

    bug/performance fixes for bytesizedring. Mainly how it handles adding multiple packets, and locking.

--- src/bytesizedring.cpp
+++ src/bytesizedring.cpp
@@ -13,7 +13,7 @@
 
 struct bs_ring* create_bsring(uint32_t capacity, int32_t socket) {
 	static volatile uint32_t ring_cnt = 0;
-	int count_min = capacity/60;
+	int count_min = 1 + capacity/60;
 	int count = 1;
 	
 	// DPDK ring buffers come with sizes of 2^n, but the actual storage limit
@@ -29,6 +29,7 @@ struct bs_ring* create_bsring(uint32_t capacity, int32_t socket) {
 	char ring_name[32];
 	struct bs_ring* bsr = (struct bs_ring*)malloc(sizeof(struct bs_ring));
 	bsr->capacity = capacity;
+	bsr->ring_locked = false;
 	sprintf(ring_name, "mbuf_bs_ring%d", __sync_fetch_and_add(&ring_cnt, 1));
 	bsr->ring = rte_ring_create(ring_name, count, socket, RING_F_SP_ENQ | RING_F_SC_DEQ);
 	bsr->bytes_used = 0;
@@ -40,6 +41,9 @@ struct bs_ring* create_bsring(uint32_t capacity, int32_t socket) {
 	return bsr;
 }
 
+/**
+ * XXX review this function.
+ */
 int bsring_enqueue_bulk(struct bs_ring* bsr, struct rte_mbuf** obj, uint32_t n) {
 	uint32_t num_added = 0;
 
@@ -48,7 +52,7 @@ int bsring_enqueue_bulk(struct bs_ring* bsr, struct rte_mbuf** obj, uint32_t n) 
 	uint32_t i = 0;
 	uint32_t total_size = 0;
 	for (i=0; i<n; i++) {
-		total_size += obj[i]->pkt_len;
+		total_size += obj[i]->pkt_len + FRAME_OVERHEAD;
 	}
 	if ((bsr->bytes_used + total_size) > bsr->capacity) {
 		// the mbufs will be dropped.  Free them.
@@ -77,38 +81,61 @@ int bsring_enqueue_bulk(struct bs_ring* bsr, struct rte_mbuf** obj, uint32_t n) 
 	// adjust the bsring's usage values
 	total_size = 0;
 	for (i=0; i<num_added; i++) {
-		total_size += obj[i]->pkt_len;
+		// we now know this is problematic
+		total_size += obj[i]->pkt_len + FRAME_OVERHEAD;
 	}
 
 	bsr->bytes_used += total_size;
 	return num_added;
 }
 
+/**
+ * Add up to n mbufs to the ring.
+ * Returns the number of mbufs actually added.
+ * mbufs not added are freed by this function.
+ * This function is safe for a single producer, but expect performance issues
+ * with multiple producers.
+ */
 int bsring_enqueue_burst(struct bs_ring* bsr, struct rte_mbuf** obj, uint32_t n) {
 	uint32_t num_to_add = 0;
 	uint32_t num_added = 0;
-	uint32_t bytes_added = 0;
 	
 	// in burst mode we add as many packets as will fit.
 	// count how many packets we can add from the start of this batch.
+	// We start by reserving space in the ring by increasing bsr->bytes_used.
 	uint32_t i = 0;
-	int bytes_remaining = bsr->capacity - bsr->bytes_used;
-	while ((i < n) && (bytes_remaining > 0)) {
-		bytes_remaining -= (obj[i]->pkt_len);
+	//bool ring_empty = (bsr->bytes_used == 0);
+	if (n>0) {
+	uint32_t bytes_to_add = obj[i]->pkt_len + FRAME_OVERHEAD;
+	while ((bsr->bytes_used == 0) || ((i < n) && ((bsr->bytes_used+bytes_to_add) < bsr->capacity))) {
+		//bsr->bytes_used += (obj[i]->pkt_len + FRAME_OVERHEAD);
+		bsr->bytes_used += bytes_to_add;
 		num_to_add++;
 		i++;
+		if (i<n) bytes_to_add = obj[i]->pkt_len + FRAME_OVERHEAD;
 	}
-	if (bytes_remaining < 0) {
+	}
+/*
+	if ((!ring_empty) && (bsr->bytes_used > bsr->capacity) && (num_to_add > 0)) {
+		//if (i <= 0) {
+		//	printf("i=%d  ring_empty=%d  num_to_add=%d  bytes_used=%u  capacity=%u\n", i, ring_empty, num_to_add, unsigned(bsr->bytes_used), unsigned(bsr->capacity));
+		//}
+		bsr->bytes_used -= (obj[i-1]->pkt_len + FRAME_OVERHEAD);
 		num_to_add--;
 	}
+*/
 	
+	// try to add the mbufs
 	num_added = rte_ring_sp_enqueue_burst(bsr->ring, (void**)obj, num_to_add, NULL);
-	for (i=0; i<num_added; i++) {
-		bytes_added += (obj[i]->pkt_len);
+
+	// if any of them failed to add, decrement for the space we didn't use
+	for (uint32_t i=num_added; i<num_to_add; i++) {
+		bsr->bytes_used -= (obj[i]->pkt_len + FRAME_OVERHEAD);
 	}
 
 	// free any mbufs that didn't make it in.
-	for (uint32_t i=num_added; i<num_to_add; i++) {
+	//for (uint32_t i=num_added; i<num_to_add; i++) {
+	for (uint32_t i=num_added; i<n; i++) {
 		rte_pktmbuf_free(obj[i]);
 		obj[i] = NULL;
 	}
@@ -117,6 +144,7 @@ int bsring_enqueue_burst(struct bs_ring* bsr, struct rte_mbuf** obj, uint32_t n)
 	// It's possible that some of the remaining frames are small enough
 	// to fit into the remaining space.  Try them iteratively.
 	// Free any mbufs that don't get added
+	/*
 	if (num_added < n) {
 		bytes_remaining = bsr->capacity - bsr->bytes_used - bytes_added;
 		i = num_to_add;
@@ -133,17 +161,32 @@ int bsring_enqueue_burst(struct bs_ring* bsr, struct rte_mbuf** obj, uint32_t n)
 			i++;
 		}
 	}
-	
+	*/
 	// XXX - We could be incrementing bsr->bytes_used as we enqueue
 	//       the mbufs instead of adding them all at the end.
-	bsr->bytes_used += bytes_added;
+	//bsr->bytes_used += bytes_added;
 	return num_added;
 }
 
+
+/**
+ * Add one mbuf to the ring.
+ * Returns the number of mbufs actually added, 0 or 1.
+ * mbufs not added are freed by this function.
+ * This function is safe for a single producer, but expect performance issues
+ * with multiple producers.
+ */
 int bsring_enqueue(struct bs_ring* bsr, struct rte_mbuf* obj) {
-	if ((bsr->bytes_used + obj->pkt_len) < bsr->capacity) {
+	if (obj == NULL) {
+		return 0;
+	}
+
+	// remember the pkt size locally.  Once we enqueue it, it can be
+	// dequeued, sent, and freed before we can account for it.
+	uint32_t pkt_size = obj->pkt_len + FRAME_OVERHEAD;
+	if ((bsr->bytes_used == 0) || ((bsr->bytes_used + pkt_size) < bsr->capacity)) {
 		if (rte_ring_sp_enqueue(bsr->ring, obj) == 0) {
-			bsr->bytes_used += (obj->pkt_len);
+			bsr->bytes_used += pkt_size;
 			return 1;
 		} else {
 			// this shouldn't happen
@@ -159,7 +202,7 @@ int bsring_dequeue_burst(struct bs_ring* bsr, struct rte_mbuf** obj, uint32_t n)
 	uint32_t i = 0;
 	if (num_dequeued > 0) {
 		for (i=0; i<num_dequeued; i++) {
-			bsr->bytes_used -= (obj[i]->pkt_len);
+			bsr->bytes_used -= (obj[i]->pkt_len + FRAME_OVERHEAD);
 		}
 	}
 	return num_dequeued;
@@ -170,7 +213,7 @@ int bsring_dequeue_bulk(struct bs_ring* bsr, struct rte_mbuf** obj, uint32_t n) 
 	uint32_t i = 0;
 	if (num_dequeued > 0) {
 		for (i=0; i<num_dequeued; i++) {
-			bsr->bytes_used -= (obj[i]->pkt_len);
+			bsr->bytes_used -= (obj[i]->pkt_len + FRAME_OVERHEAD);
 		}
 	}
 	return num_dequeued;
@@ -178,7 +221,7 @@ int bsring_dequeue_bulk(struct bs_ring* bsr, struct rte_mbuf** obj, uint32_t n) 
 
 int bsring_dequeue(struct bs_ring* bsr, struct rte_mbuf** obj) {
 	if (rte_ring_sc_dequeue(bsr->ring, (void**)obj) == 0) {
-		bsr->bytes_used -= (obj[0]->pkt_len);
+		bsr->bytes_used -= (obj[0]->pkt_len + FRAME_OVERHEAD);
 		return 1;
 	}
 	return 0;
